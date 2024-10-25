4dbe9f9cc5db28cc6975f721e8d1cc3344baea69
alokkr016 <alokraj016@outlook.com>
2024-10-25 23:36:31 +0530

    Created all APIs

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ let cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.static('static'));
-const port = 3010;
+const port = 3000;
 
+let tasks = [
+  { taskId: 1, text: 'Fix bug #101', priority: 2 },
+  { taskId: 2, text: 'Implement feature #202', priority: 1 },
+  { taskId: 3, text: 'Write documentation', priority: 3 }
+];
 
 app.get('/', (req, res) => {
  
